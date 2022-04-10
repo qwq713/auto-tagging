cd90c3738ccaf3fa343e9d2ccb0799540874645a
DaengMa <qwq713@hotmail.com>
2022-04-11 00:02:37 +0900

    update make_zipfile.sh

--- make_zipfile.sh
+++ make_zipfile.sh
@@ -6,10 +6,12 @@ FAIL=1
 zipfile="lambda.zip"
 zip -r $zipfile ./module ./main.py ./lambda_function.py
 
-if [ -f "$zipFile" ]; then  
-    exit $SUCCESS
-else
-    exit $FAIL
-fi
+# sleep 1
+
+# if [ -f "./$zipFile" ]; then  
+#     exit $SUCCESS
+# else
+#     exit $FAIL
+# fi
 
 # aws lambda update-function-code --function-name auto-tagging --zip-file fileb://$zipfile --profile TEST
