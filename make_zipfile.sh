77bd33d45b248dc77133046329b209c345c18f04
DaengMa <qwq713@hotmail.com>
2022-04-11 23:08:33 +0900

    [20220411] codedeploy용 appspec.yml 작성

--- make_zipfile.sh
+++ make_zipfile.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
 
-SUCCESS=0
-FAIL=1
-
 zipfile="lambda.zip"
 zip -r $zipfile ./module ./main.py ./lambda_function.py
-
-# sleep 1
-
-# if [ -f "./$zipFile" ]; then  
-#     exit $SUCCESS
-# else
-#     exit $FAIL
-# fi
-
-# aws lambda update-function-code --function-name auto-tagging --zip-file fileb://$zipfile --profile TEST
